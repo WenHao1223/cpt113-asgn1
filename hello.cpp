1e1f174d7d4f3fc45a19ec03da5eb711b448eb64
Lim Wen Hao <wenhaojshs@gmail.com>
2024-05-08 15:53:54 +0800

    initial setup

--- hello.cpp
+++ hello.cpp
@@ -2,21 +2,5 @@
 using namespace std;
 
 int main () {
-  cout << "Hello, World!" << endl;
-
-  cout << "dev";
-
-  cout << "Lim Wen Hao is sabi";
-
-  cout << "Assignmenttttt";
-
-  cout << "Teh  Juin Ewe is here to sabotage";
-  cout << "Welcome handsome boy";
-
-  for (int i = 0; i < 10; i++) {
-    cout << "WenHao" << endl; cout << "Hello, World!";
-  }
-
-  cout << "Now is juinewe branch edit, hihihi";
   return 0;
 }
